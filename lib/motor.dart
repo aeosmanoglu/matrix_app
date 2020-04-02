c2d9714587b6083aad5deaf5231456116ef0f0f2
Abuzer Emre <a.osmanoglu@jandarma.gov.tr>
2020-04-02 16:53:50 +0300

    logout

--- lib/motor.dart
+++ lib/motor.dart
@@ -4,15 +4,28 @@ import 'package:matrixapp/login_obj.dart';
 
 class Motor {
   String server = "http://localhost:8008";
-  Map<String, String> header = {"content-type": "application/json"};
 
   login(String user, String password) async {
     String url = server + "/_matrix/client/r0/login";
+    Map<String, String> headers = {"content-type": "application/json"};
     String type = "m.login.password";
     Map<String, String> identifier = {"type": "m.id.user", "user": user};
     String body = jsonEncode({"type": type, "identifier": identifier, "password": password});
-    Response response = await post(url, headers: header, body: body);
+    Response response = await post(url, headers: headers, body: body);
     var obj = LoginObj.fromJson(jsonDecode(response.body));
     return obj;
+  }
+
+  logout(String accessToken) async {
+    String url = server + "/_matrix/client/r0/logout";
+    Map<String, String> headers = {"authorization": "Bearer $accessToken"};
+    Response response = await post(url, headers: headers);
+    print(response.statusCode);
+  }
+
+  logoutAll(String accessToken) async {
+    String url = server + "/_matrix/client/r0/logout/all";
+    Map<String, String> headers = {"Authorization": "Bearer $accessToken"};
+    await post(url, headers: headers);
   }
 }
