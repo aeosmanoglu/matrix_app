ad5567952cb798c9f729d34d195077c3af848211
Abuzer Emre <a.osmanoglu@jandarma.gov.tr>
2020-04-03 15:03:36 +0300

    user search

--- lib/motor.dart
+++ lib/motor.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:matrixapp/login_obj.dart';
+import 'package:matrixapp/user_search_obj.dart';
 
 class Motor {
   String server = "http://localhost:8008";
@@ -12,7 +13,7 @@ class Motor {
     Map<String, String> identifier = {"type": "m.id.user", "user": user};
     String body = jsonEncode({"type": type, "identifier": identifier, "password": password});
     Response response = await post(url, headers: headers, body: body);
-    var obj = LoginObj.fromJson(jsonDecode(response.body));
+    LoginObj obj = LoginObj.fromJson(jsonDecode(response.body));
     return obj;
   }
 
@@ -27,5 +28,14 @@ class Motor {
     String url = server + "/_matrix/client/r0/logout/all";
     Map<String, String> headers = {"Authorization": "Bearer $accessToken"};
     await post(url, headers: headers);
+  }
+
+  userSearch(String accessToken, String searchTerm) async {
+    String url = server + "/_matrix/client/r0/user_directory/search";
+    Map<String, String> headers = {"content-type": "application/json", "authorization": "Bearer $accessToken"};
+    String body = jsonEncode({"search_term": searchTerm});
+    Response response = await post(url, headers: headers, body: body);
+    UserSearchObj obj = UserSearchObj.fromJson(jsonDecode(response.body));
+    return obj.results;
   }
 }
